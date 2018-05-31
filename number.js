59cd8e7974e31d96d6799b3bec1ee82907639b4e
jeromedennisbiz <jeromedennis.biz@gmail.com>
2018-05-31 10:10:49 +0530

    about JS Number Operations

---
+++ number.js
@@ -0,0 +1,6 @@
+let studentScore = 18;
+var maxScore = 25;
+let studentScorePercent;
+
+studentScorePercent = parseFloat(studentScore / maxScore) * 100;
+console.log('Student Score Percent : %d %', studentScorePercent);
